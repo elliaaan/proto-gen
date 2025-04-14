0d8ad3fed2926e5dad55bbe83568f417f6d3c85a
elliaaan <230745@astanait.edu.kz>
2025-04-15 02:42:24 +0500

    feat: update generated pb files for inventory and order

--- generate.go
+++ generate.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate protoc --proto_path=../protos/inventory --go_out=. --go-grpc_out=. ../protos/inventory/inventory.proto
-//go:generate protoc --proto_path=../protos/order --go_out=. --go-grpc_out=. ../protos/order/order.proto
+//go:generate protoc --proto_path=../protos/inventory --go_out=pb/inventory --go-grpc_out=pb/inventory ../protos/inventory/inventory.proto
+//go:generate protoc --proto_path=../protos/order --go_out=pb/order --go-grpc_out=pb/order ../protos/order/order.proto
 
 func main() {}
