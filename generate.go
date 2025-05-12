589299429611732700fc089d4e3a1077bc6493a0
elliaaan <230745@astanait.edu.kz>
2025-05-12 18:53:54 +0500

    Initial commit: proto-gen setup

--- generate.go
+++ generate.go
@@ -2,5 +2,7 @@ package main
 
 //go:generate protoc --proto_path=../protos/inventory --go_out=./pb/inventory --go-grpc_out=./pb/inventory ../protos/inventory/inventory.proto
 //go:generate protoc --proto_path=../protos/order --go_out=./pb/order --go-grpc_out=./pb/order ../protos/order/order.proto
+//go:generate protoc --proto_path=../protos/statistics --go_out=./pb/statistics --go-grpc_out=./pb/statistics ../protos/statistics/statistics.proto
+//go:generate protoc --proto_path=../protos/order --go_out=./pb/order --go-grpc_out=./pb/order ../protos/order/statistics.proto
 
 func main() {}
